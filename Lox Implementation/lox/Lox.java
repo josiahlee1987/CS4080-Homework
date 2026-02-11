e6faf75234cd7b03c2de2899c9d4b2dd9a960619
josiahlee1987 <josiahlee@cpp.edu>
2026-02-11 13:54:41 -0800

    Updated code (2.11)

--- Lox Implementation/lox/Lox.java
+++ Lox Implementation/lox/Lox.java
@@ -10,7 +10,10 @@ import java.util.List;
 
 // Lox.main()
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -21,50 +24,67 @@ public class Lox {
             runPrompt();
         }
     }
-}
 
-// --------------- WRAPPERS ---------------
+    // --------------- WRAPPERS ---------------
 // Execute from file (file argument)
-private static void runFile(String path) throws IOException {
-    byte[] bytes = Files.readAllBytes(Paths.get(path));
-    run(new String(bytes, Charset.defaultCharset()));
-    // Indicate an error in the exit code.
-    if (hadError) System.exit(65);
-}
-
-// Execute as interactable (code in console)
-private static void runPrompt() throws IOException {
-    InputStreamReader input = new InputStreamReader(System.in);
-    BufferedReader reader = new BufferedReader(input);
-
-    for (;;) {
-        System.out.print("> ");
-        String line = reader.readLine();
-        if (line == null) break;
-        run(line);
-        hadError = false;
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        // Indicate an error in the exit code.
+        if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
-}
 
-// --------------- run() tokenizer ---------------
-private static void run(String source) {
-    Scanner scanner = new Scanner(source);
-    List<Token> tokens = scanner.scanTokens();
+    // Execute as interactable (code in console)
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
 
-    // For now, just print the tokens.
-    for (Token token : tokens) {
-        System.out.println(token);
+        for (; ; ) {
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false;
+        }
     }
-}
 
-// --------------- ERROR HANDLING ---------------
-static void error(int line, String message) {
-    report(line, "", message);
-}
+    // --------------- run() tokenizer ---------------
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
 
-private static void report(int line, String where,
-        String message) {
-    System.err.println(
-            "[line " + line + "] Error" + where + ": " + message);
-    hadError = true;
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        interpreter.interpret(expression);
+    }
+
+    // --------------- ERROR HANDLING ---------------
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() +
+                "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
+
+    private static void report(int line, String where,
+                               String message) {
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
 }
